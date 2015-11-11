49596b4d90e57f754635951e75b66e3d858460e8
GingerAebi <GingerAebi@gmail.com>
2015-11-11 20:49:54 +0900

    #15 JSON을 위해 json - simple 라이브러리 추가 data.json 오류 수정 BlockValidate 체크를 위한 테스트 + 코드 작성 DB작성을 위한 Dao 클래스 생성

--- Novelizer/src/controller/block/BlockValidateChecker.java
+++ Novelizer/src/controller/block/BlockValidateChecker.java
@@ -1,8 +1,79 @@
 package controller.block;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class BlockValidateChecker {
 	public boolean isValidate(String jsonData){
 		
+		JSONParser parser = new JSONParser();
+		
+		try {
+			JSONArray blockListArray = (JSONArray)parser.parse(jsonData);
+			for(int i = 0 ; i < blockListArray.size() ; i++){
+				JSONObject blockObject = (JSONObject)blockListArray.get(i);
+				System.out.println(blockObject);
+				if(!isBlockHasEssentialData(blockObject)){ return false; }
+			}
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+	
+	private boolean isBlockHasEssentialData(JSONObject blockObject){
+		long blockId = (long)blockObject.get("blockId");
+		long nextBlockId = (long)blockObject.get("nextBlockId");
+		JSONArray actionArray = (JSONArray)blockObject.get("actionList");
+		
+		if(isValidateBlockId(blockId) && isValidateBlockId(nextBlockId) && isValidateActionList(actionArray)){
+			return true;
+		}else{
+			System.out.println("Block Data Error");
+			return false;
+		}
+			
+		
+	}
+	
+	private boolean isValidateBlockId(long blockId){
+		if(blockId >= 0){
+			return true;
+		}
+		else{
+			System.out.println("Block Id is invalidate");
+			return false;
+		}
+	}
+	
+	private boolean isValidateActionList(JSONArray actionArray){
+		for(int i = 0 ; i < actionArray.size() ;i++){
+			JSONObject actionObject = (JSONObject) actionArray.get(i);
+			if(!isActionHasEssentialData(actionObject)) return false;
+		}
+		return true;
+	}
+	
+	private boolean isActionHasEssentialData(JSONObject actionObject){
+		String actionType = (String) actionObject.get("type");
+		
+		if(isValidateActionType(actionType)){
+			return true;
+		}
 		return false;
 	}
+	
+	private boolean isValidateActionType(String actionType){
+		if(actionType.equals("text")||actionType.equals("character")||actionType.equals("background")){
+			return true;
+		}else{
+			System.out.println("ActionType is invalidate");
+			return false;
+		}
+	}
+	
 }
