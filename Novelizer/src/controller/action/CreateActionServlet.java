d44668dcbaef94437debb1a463bd033290202c02
HH <doubleh777@nhnnext.org>
2015-11-17 15:54:58 +0900

    CreateActionServlet 수정

--- Novelizer/src/controller/action/CreateActionServlet.java
+++ Novelizer/src/controller/action/CreateActionServlet.java
@@ -2,6 +2,7 @@ package controller.action;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,23 +11,75 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.block.BlockController;
+import model.dao.ActionDAO;
+import vo.action.Action;
+import vo.action.BackgroundAction;
+import vo.action.CharacterAction;
+import vo.action.TextAction;
 
-@WebServlet("/postBlockInfo")
+@WebServlet("/createAction")
 public class CreateActionServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		int sceneId = Integer.parseInt(req.getParameter("sceneId"));
-		int blockId = Integer.parseInt(req.getParameter("blockId"));
+		ActionValidateChecker checker = new ActionValidateChecker();
+		ActionDAO dao = new ActionDAO();
 		
-		BlockController bc = new BlockController();
+		if(!checker.isValid(req)){
+			PrintWriter writer = resp.getWriter();
+			writer.write("Error : param is not valid");
+			};
+
+		dao.saveActionData(getActionObj(req));
 		
-		String nextBlockInfo = bc.getNextBlock(sceneId, blockId);
-		
-		ObjectOutputStream dataOutPut = new ObjectOutputStream(resp.getOutputStream());
-		dataOutPut.writeUTF(nextBlockInfo);
-		dataOutPut.flush();
-		dataOutPut.close();
-		
+	}
+
+	private Action getActionObj(HttpServletRequest req) {
+		String type = req.getParameter("type");
+	
+		switch(type){
+		case "BackgroundAction":
+			return getBackgroundActionObj(req);
+		case "CharacterAction" : 
+			return getCharacterActionObj(req);
+		case "TextAction" :
+			return getTextActionObj(req);
+	}
+		/* ActionValidateChecker에서 이미 체크하여 null로 오는 경우 없음 */
+		return null;
+	}
+
+	private Action getTextActionObj(HttpServletRequest req) {
+		int id = Integer.parseInt(req.getParameter("id"));
+		String type = req.getParameter("type");
+		String text = req.getParameter("text");
+		int characterId = Integer.parseInt(req.getParameter("characterId"));
+		return new TextAction(id, type, text, characterId);
+	}
+
+	private Action getCharacterActionObj(HttpServletRequest req) {
+		int id = Integer.parseInt(req.getParameter("id"));
+		String type = req.getParameter("type");
+		int characterId = Integer.parseInt(req.getParameter("characterId"));
+		int presetId = Integer.parseInt(req.getParameter("presetId"));
+		String option = req.getParameter("option");
+		int animation = Integer.parseInt(req.getParameter("animation"));
+		int[] position = parsePosition(req.getParameter("position"));
+		return new CharacterAction(id, type, characterId, presetId, option, animation, position);
+	}
+
+	private Action getBackgroundActionObj(HttpServletRequest req) {
+		int id = Integer.parseInt(req.getParameter("id"));
+		String type = req.getParameter("type");
+		int presetId = Integer.parseInt(req.getParameter("presetId"));
+		String option = req.getParameter("option");
+		int animation = Integer.parseInt(req.getParameter("animation"));
+		int[] position = parsePosition(req.getParameter("position"));
+		return new BackgroundAction(id, type, presetId, option, animation, position);
+	}
+	
+	private int[] parsePosition(String parameter) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
