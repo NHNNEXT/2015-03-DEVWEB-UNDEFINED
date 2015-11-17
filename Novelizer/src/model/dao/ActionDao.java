324c3e94d3a9942b713e957966dc2596f38390f9
HH <doubleh777@nhnnext.org>
2015-11-18 02:00:08 +0900

    ActionDao update

--- Novelizer/src/model/dao/ActionDao.java
+++ Novelizer/src/model/dao/ActionDao.java
@@ -1,10 +1,13 @@
 package model.dao;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
 
 import vo.action.Action;
 
 public class ActionDao {
+	private DB db;
 
 	public ActionDao() {
 		Mongo conn = utils.Connection.getConnection();
@@ -12,6 +15,12 @@ public class ActionDao {
 	}
 	
 	public void saveActionData(Action actionObj) {
+		DBCollection actionCollection = db.getCollection("action");
+		actionCollection.drop();
+		insertActionData(actionCollection, actionObj);
+	}
+
+	private void insertActionData(DBCollection actionCollection, Action actionObj) {
 				
 	}
 
