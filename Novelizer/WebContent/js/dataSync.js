6f825b5a5cd52e1e8aa2d2c3f53d33952fd1ca89
CHANT-MAC <cca_company@naver.com>
2015-11-24 20:29:18 +0900

    클라이언트 리팩토링, 뷰어 서버 연동 테스트

--- Novelizer/WebContent/js/dataSync.js
+++ Novelizer/WebContent/js/dataSync.js
@@ -3,6 +3,10 @@
  * author : cca-company
  */
 
+/*
+ * callback = {done:callbackDoneFunc, fail:callbackFailFunc}
+ */
+
 var EditorDataSync = {
  	url : "http://127.0.0.1:8080",
  	blockList : [],
@@ -16,51 +20,49 @@ var EditorDataSync = {
 		//this.IDBRequest.onupgradeneeded = this.upgradeDB;
  	},
  	loadData : function(data){
- 		EditorDataSync.blockList = data;
+ 		this.blockList = data;
  		// TODO : 서버에서 받은 기존 리스트 정보를 DOM에 추가
  	},
- 	getSceneData : function(callbackDone,callbackFail){	// 현재 씬의 블록 리스트 데이터를 가져옴
- 		var request = $.ajax({
- 			url : this.url+"/getBlock.do",
+ 	getSceneData : function(callback){	// 현재 씬의 블록 리스트 데이터를 가져옴
+ 		$.ajax({
+ 			url : this.url+"/blockList",
  			method : "GET",
  			dataType : "json",
  			data : {sceneId : this.sceneId},
  		})
-
- 		request.done(callbackDone);
- 		request.fail(callbackFail);
+ 		.done(callback[done])
+ 		.fail(callback[fail]);
  	},
- 	postSceneData : function(callbackDone,callbackFail){
- 		var request = $.ajax({
- 			url : this.url+"/postBlock.do",
+ 	postSceneData : function(callback){
+ 		$.ajax({
+ 			url : this.url+"/blockList",
  			method : "POST",
  			dataType : "json",
  			data : {sceneId : this.sceneId,
  					data : JSON.stringify(this.blockList)},
  		})
-
- 		request.done(callbackDone);
- 		request.fail(callbackFail);
+		.done(callback[done])
+		.fail(callback[fail]);
  	},
- 	addBlock : function(callbackDone, callbackFail){
+ 	addBlock : function(callbackDone){
  		var maxIdx = this.blockList.length-1;
  		var maxBlockId = 0;
  		if(maxIdx >= 0){			
  			maxBlockId = this.blockList[maxIdx].blockId;
  			this.blockList[maxIdx].nextBlockId=maxBlockId+1;
  		}
-
  		var data = {
- 			blockId : maxBlockId+1,
+ 			blockId : ++maxBlockId,
  			actionList : [],
  			nextBlockId : 0,
  		};
- 		this.blockList.push(data);
 
+ 		this.blockList.push(data);
  		callbackDone(data);
+ 		return maxBlockId;
  	},
- 	removeBlock : function(blockId, callbackDone, callbackFail){
- 		var targetBlockIdx = this.getBlock(blockId,true);
+ 	removeBlock : function(blockId, callback){
+ 		var targetBlockIdx = this.blockList.indexOf(this.getBlock(blockId));
  		var prevBlock = this.getPrevBlock(blockId);
 		var targetBlock = this.blockList.splice(targetBlockIdx,1);
  		
@@ -69,7 +71,7 @@ var EditorDataSync = {
  		}
 
  	},
- 	sortBlock : function(blockId, prevBlockId, callbackDone, callbackFail){
+ 	sortBlock : function(blockId, prevBlockId, callback){
  		var targetBlock = this.getBlock(blockId);
  		var currentPrevBlock = this.getBlock(prevBlockId);
  		var beforePrevBlock = this.getPrevBlock(blockId);
@@ -79,67 +81,69 @@ var EditorDataSync = {
  		currentPrevBlock.nextBlockId = blockId;
 
  	},
- 	addAction : function(blockId, callbackDone, callbackFail){
+ 	sortAction : function(blockId, actionId, changeBlockId){
+ 		var block = this.getBlock(blockId);
+ 		var actionIdx = block.actionList.indexOf(this.getAction(blockId, actionId));
+ 		var action = block.actionList.splice(actionIdx,1);
+ 		var changeBlock = this.getBlock(changeBlockId);
+ 		var maxIdx = block.actionList.length-1;
+ 		var maxActionId = (block.actionList[maxIdx]) ? block.actionList[maxIdx].actionId : 0;
+
+ 		action.blockId = changeBlockId;
+ 		action.actionId = ++maxActionId;
+ 		changeBlock.actionList.push(action);
+
+ 		return action;
+ 	},
+ 	addAction : function(blockId, callbackDone){
  		var targetBlock = this.getBlock(blockId);
  		var maxIdx = targetBlock.actionList.length-1;
  		var maxActionId = (targetBlock.actionList[maxIdx]) ? targetBlock.actionList[maxIdx].actionId : 0;
  		var data = {
- 			actionId : maxActionId+1,
- 			actionType : "text",
+ 			blockId : blockId,
+ 			actionId : ++maxActionId,
+ 			type : "TEXT",
  			text : "임시 데이터"
  		};
+
  		targetBlock.actionList.push(data);
  		callbackDone(data);
+ 		return maxActionId;
  	},
- 	getBlock : function(blockId, getIdx){
- 		if(!getIdx) getIdx = false;
+ 	modifyAction : function(blockId, actionId, actionData){
+ 		var block = this.getBlock(blockId);
+ 		var actionIdx = block.actionList.indexOf(this.getAction(blockId,actionId));
 
- 		// ISSUE! javascript로 더 나은 성능의 search하는 방법은? 
+ 		actionData.actionId = actionId;
+ 		actionData.blockId = blockId;
+ 		block.actionList[actionIdx] = actionData;
+ 	},
+ 	getBlock : function(blockId){
  		for(var i = 0; i < this.blockList.length; ++i){
  			if(this.blockList[i].blockId == blockId){
- 				if(getIdx == true){
- 					return i;
- 				}else{
  					return this.blockList[i];
- 				}
  			}
  		}
- 		console.log("blockId="+blockId+" 해당 block데이터를 찾을수 없습니다"); 		
- 		if(getIdx == true){
- 			return -1;
-		}else{
- 			return null;
-		}
+ 		console.log("blockId="+blockId+" 해당 block데이터를 찾을수 없습니다"); 	
+ 		return null;
  	},
- 	getPrevBlock : function(blockId, getIdx){
- 		if(!getIdx) getIdx = false;
-
+ 	getPrevBlock : function(blockId){
  		for(var i = 0; i < this.blockList.length; ++i){
  			if(this.blockList[i].nextBlockId == blockId){
- 				if(getIdx == true){
- 					return i;
- 				}else{
- 					return this.blockList[i];
- 				}
+ 				return this.blockList[i];
  			}
  		}
  		console.log("nextBlockId="+blockId+" 해당 block데이터를 찾을수 없습니다"); 		
- 		if(getIdx == true){
- 			return -1;
-		}else{
- 			return null;
-		}
- 	}
+ 		return null;
+ 	},
+ 	getAction : function(blockId, actionId){
+ 		var block = this.getBlock(blockId);
+ 		for(var i = 0; i < block.actionList.length; ++i){
+ 			if(block.actionList[i].actionId == actionId){
+ 					return block.actionList[i];
+ 			}
+ 		}
+ 		console.log("actionId="+actionId+" 해당 action데이터를 찾을수 없습니다"); 	
+ 		return null;
+ 	},
 }
-
-/*
-function Dog(){
-	this.name = "aaa";
-}
-
-Dog.prototype.getName = function(){
-
-}
-
-new Dog();
-*/
--- Novelizer/WebContent/js/viewer.js
+++ Novelizer/WebContent/js/viewer.js
@@ -1,121 +1,41 @@
 var Viewer = {
+	url : "http://127.0.0.1:8080",
 	sceneId : 1,
 	blockList : [],
 	currentBlock : null,
 	presetData : ["http://placekitten.com/g/200/500","http://placekitten.com/g/205/500","http://placehold.it/1000x600"],//임시 데이터
 	init : function(){
-		this.getSceneData(this.startScene);
-		$(document).on("click","#layer-text",this.onCommand);
+		this.getSceneData(this.startScene.bind(this));
+		$(document).on("click","#layer-text",this.onCommand.bind(this));
 	},
 	onCommand : function(){
-		var nextBlock = Viewer.getNextBlock(Viewer.currentBlock);
+		var nextBlock = this.getNextBlock(this.currentBlock);
 		if(nextBlock){
-			Viewer.playBlock(nextBlock);
+			this.playBlock(nextBlock);
 		}else{
 			alert("끝입니다");
 		}
 	},
 	getSceneData : function(callbackDone){
-		/*
  		var request = $.ajax({
- 			url : this.url+"/getBlock.do",
+ 			url : this.url+"/blockList",
  			method : "GET",
  			dataType : "json",
  			data : {sceneId : this.sceneId},
  		})
 
  		request.done(callbackDone);
-		*/
-		// 임시 데이터 하드코딩...ㅋㅋ 
-		callbackDone([
-{
-	"blockId" : 1,
-	"nextBlockId" : 2,
-	"actionList":[
-		{
-			"type":"CHARACTER",
-			"characterId":1,
-			"presetId":1,
-			"option":"APPEAR",
-			"position":[100,0],
-			"animation":null
-		},
-		{
-			"type":"BACKGROUND",
-			"presetId":3,
-			"option":"APPEAR",
-			"position":[0,0],
-			"animation":null
-		}
-	]
-},
-{
-	"blockId" : 2,
-	"nextBlockId" : 3,
-	"actionList":[
-		{
-			"type":"TEXT",
-			"text":"왜 이렇게 철수가 늦지?",
-			"characterId":1
-		}
-	]
-},
-{
-	"blockId" : 3,
-	"nextBlockId" : 4,
-	"actionList":[
-		{
-			"type":"CHARACTER",
-			"characterId":2,
-			"presetId":2,
-			"option":"APPEAR",
-			"position":[700,0],
-			"animation":null
-		},
-		{
-			"type":"TEXT",
-			"text":"헉헉, 미안해 내가 너무 늦었지?",
-			"characterId":2
-		}
-	]
-},
-{
-	"blockId" : 4,
-	"nextBlockId" : 5,
-	"actionList":[
-		{
-			"type":"TEXT",
-			"text":"그걸 지금 말이라고 해? 우리 헤어져! ",
-			"characterId":1
-		}
-	]
-},
-{
-	"blockId" : 5,
-	"nextBlockId" : 0,
-	"actionList":[
-		{
-			"type":"CHARACTER",
-			"characterId":1,
-			"presetId":1,
-			"option":"DISAPPEAR",
-			"position":[0,0],
-			"animation":null
-		}
-	]
-}
-]);
 	},
 	startScene : function(data){
-		Viewer.blockList = data;
-		var block = Viewer.getBlock(1); // 임시 데이터
-		Viewer.playBlock(block);
+		this.blockList = data;
+		var block = this.getBlock(1); // 임시 데이터
+		this.playBlock(block);
 	},
 	playBlock : function(block){
-		Viewer.currentBlock = block;
+		this.currentBlock = block;
 		for(var actionIdx in block.actionList){
 			var action = block.actionList[actionIdx];
-			Viewer.doAction(action);
+			this.doAction(action);
 		}
 	},
 	doAction : function(action){
@@ -133,7 +53,7 @@ var Viewer = {
 			if(action.option == "APPEAR"){
 				$("<div id='character"+action.characterId+"' class='character'></div>")
 					.css({
-						backgroundImage:"url('"+Viewer.presetData[action.presetId-1]+"')",
+						backgroundImage:"url('"+this.presetData[action.presetId-1]+"')",
 						width:"200px",
 						height:"500px",
 						left:action.position[0],
@@ -148,29 +68,19 @@ var Viewer = {
 		{
 			layer = $("#layer-bg");
 			layer.css({
-				backgroundImage:"url('"+Viewer.presetData[action.presetId-1]+"')",
+				backgroundImage:"url('"+this.presetData[action.presetId-1]+"')",
 			})
 		}
 	},
- 	getBlock : function(blockId, getIdx){
- 		if(!getIdx) getIdx = false;
-
- 		// ISSUE! javascript로 더 나은 성능의 search하는 방법은? 
+ 	getBlock : function(blockId){
  		for(var i = 0; i < this.blockList.length; ++i){
  			if(this.blockList[i].blockId == blockId){
- 				if(getIdx == true){
- 					return i;
- 				}else{
- 					return this.blockList[i];
- 				}
+ 				return this.blockList[i];
  			}
  		}
  		console.log("blockId="+blockId+" 해당 block데이터를 찾을수 없습니다"); 		
- 		if(getIdx == true){
- 			return -1;
-		}else{
- 			return null;
-		}
+
+ 		return null;
  	},
 	getNextBlock : function(block){
 		var nextBlockId = block.nextBlockId;
@@ -178,7 +88,7 @@ var Viewer = {
 			// TODO : 플래그 처리 
 		}
 		
-		return Viewer.getBlock(nextBlockId);
+		return this.getBlock(nextBlockId);
 	}
 }
 
