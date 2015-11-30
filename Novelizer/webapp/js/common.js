e1eca7216c536d5bb9a894289458947eabc47021
Junnie-Jobs <dj0999@naver.com>
2015-11-30 22:48:36 +0900

    툴박스 탭, 툴박스 이미지 드래그, 액션 추가했을 때 옵션 선택할 수 있는 버튼 추가.

--- Novelizer/webapp/js/common.js
+++ Novelizer/webapp/js/common.js
@@ -1,83 +1,120 @@
+//해결해야할 문제
+//1. 블록 추가시 액션 타입 지정 및 텍스트 추가/연동
+// ->현재 타입지정은 select 문으로 지정했음. 여기에 텍스트 추가를 위해 <textarea>를 어디에 추가해야할까? 그리고 프리뷰와 연동하는 방법은?
+//2. action list간 아이템 이동 가능하게 수정하기
+//-> 암만해도 action리스트를 sortable하는게 되질 않는다 문제가 무엇일까?
 
-var Editor = {
+var Editor = { //Editor 객체 형성
     selectedBlockId : 0,
-    init : function(){
+    selectedActionId : 0,
+    init : function(){ //init이라는 메서드 형성 
+        
         // 새로운 블록,액션 추가 
-        $(document).on("click","#button-add-block", this.addBlock);
-        $(document).on("click","#button-add-action", this.addAction);
+        $(document).on("click","#button-add-block", this.addBlock); //버튼 추가 버튼을 누르면 addBlock 메서드 실행    
+        $(document).on("click","#button-add-action", this.addAction); //액션 추가 버튼을 누르면 addAction 메서드 실행   
+
 
         // 블록 선택
-        $(document).on("click","#block-list li.block", this.selectBlock);
-        $(document).on("click","#button-remove", this.removeBlock);
-
-        // TODO: 액션 선택 추가
+        $(document).on("click","#block-list li.block", this.selectBlock); //블락리스트를 누르면 블럭 선택 
+        $(document).on("click","#button-remove", this.removeBlock); //제거 버튼을 누르면 블락을 제거  
 
 
-        // TODO: sortable 새로 추가된 액션 리스트에 적용되게 수정하기
+        // 액션 선택 
+        $(document).on("click","ul.action-list li", this.selectAction); //액션 리스트를 클릭하면, 액션 선택이 됨
+        $(document).on("click","ul.action-list li", this.unselectedBlock);
+        
+
+        // TODO: sortable 새로 추가된 액션 리스트에  적용되게 수정하기
         // action-list li 태그를 이동할 수 있게 
-        $( "#block-list,.action-list,#temp-action-list" ).sortable({
-            revert: true
+        $( "#block-list" ).sortable({
+            revert: true //revert는 디폴트가 false이다. 드래그가 스탑되면 원래 자리로 돌아간다. revert가 true이면 그 요소는 언제나 되돌아 간다.
         });
 
-        $( "#draggable" ).draggable({
-            connectToSortable: "#sortable",
-            helper: "clone",
-            revert: "valid"
+        $( ".action-list" ).sortable({
+            revert: true //revert는 디폴트가 false이다. 드래그가 스탑되면 원래 자리로 돌아간다. revert가 true이면 그 요소는 언제나 되돌아 간다.
         });
-        $( "ul, li" ).disableSelection();
 
-
+        $( ".draggable" ).draggable({ // 아래의 내용이 있으니 drag가 안되서 일단 주석처리함.
+            //connectToSortable: "#sortable", 
+            //helper: "clone", //클론을 하게 되면 그 요소는 복제되어 드래그 된다.
+            //revert: "valid"
+        });
     },
+
     addBlock : function(){
         EditorDataSync.addBlock(function(data){
             $("ol#block-list").append("<li data-block-id='"+data.blockId+"' class='block'><ul class='action-list'></ul></li>");
-        });
+        }); // 체이닝할 때의 타겟 
     },
     removeBlock : function(){
         EditorDataSync.removeBlock(Editor.selectedBlockId);
         $("li.selected").remove();
     },
 
-    //TODO: removieAction
+    //TODO: removeAction
 
-    //
+    //블록선택 
     selectBlock : function(){
-        Editor.selectedBlockId = $(this).data("blockId");
+        Editor.selectedBlockId = $(this).data("blockId"); //jQuery( ":data(key)" ) key: The data key.
         $("li.selected").removeClass("selected");
-        $(this).addClass("selected");
+        $(this).addClass("selected"); // 매개변수인 selected를 클래스 특성에 추가한다. 이미 존재하는 class 특성 값에 새로운 값 추가
     },
-    addAction : function(){
-        EditorDataSync.addAction(Editor.selectedBlockId,function(data){
-            $("li.selected ul.action-list").append("<li data-action-id='"+data.actionId+"'>action"+data.actionId+"</li>");  
-        });
-    }, 
+
+
     selectAction : function(){
          Editor.selectedActionId = $(this).data("actionId");
-        $("li.selected").removeClass("selected");
-        $(this).addClass("selected");
-    }
+        $(this).removeClass("selected");
+        console.log(Editor.selectedActionId);
+    },
+
+
+    addAction : function(){
+        EditorDataSync.addAction(Editor.selectedBlockId,function(data){
+           var select = $("select.temp-action-option").clone()
+           select.removeClass("temp-action-option");
+           var li = $("<li></li>");
+           li.data("actionId",data.actionId).append(select);
+           $("li.selected ul.action-list").append(li);
+          
+        });
+
+    }, 
 
 
 }
 
-$(".tab-edit").on("click",function(event){
-	switchTab("edit");
+$(".tab-edit").on("click",function(event){ //클릭하면 edit으로 넘어감.
+    switchTab("edit");
 })
-$(".tab-preset").on("click",function(event){
-	switchTab("preset");
+$(".tab-preset").on("click",function(event){ // 클릭하면 preset으로 넘어감. 
+    switchTab("preset");
 })
-$(".tab-scenario").on("click",function(event){
-	switchTab("scenario");
+$(".tab-scenario").on("click",function(event){ // 클릭하면 scenario로 넘어감. 
+    switchTab("scenario");
 })
 
+//툴 박스에서의 탭 전환
+ $(function () {
+    $(".tab_content").hide();
+    $(".tab_content:first").show();
+    $("ul.tabs li").click(function () {
+        $("ul.tabs li").removeClass("active").css("color", "#333");
+        $(this).addClass("active").css("color", "darkred");
+        $(".tab_content").hide()
+        var activeTab = $(this).attr("rel");
+        $("#" + activeTab).fadeIn()
+    });
+});
+
+
+//탭바꾸기 
 function switchTab(tabid){
-	var tabList = document.querySelectorAll("main>div");
-	for(var i = 0; i < tabList.length; ++i){
-		tabList[i].style.display="none";
-	}
-	
-	document.querySelector("#"+tabid).style.display="block";
+    var tabList = document.querySelectorAll("main>div");
+    for(var i = 0; i < tabList.length; ++i){
+        tabList[i].style.display="none";
+    }
+    
+    document.querySelector("#"+tabid).style.display="block";
 }
-
 
 
