93d9b3e2284a6be6d476255ecb3dd6adf45e5f52
CHANT-MAC <cca_company@naver.com>
2015-12-02 23:17:30 +0900

    프리셋 폴더 세팅

---
+++ Novelizer/src/controller/preset/PresetController.java
@@ -0,0 +1,38 @@
+package controller.preset;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/preset")
+public class PresetController extends HttpServlet{
+	
+	@Override
+	public void init() throws ServletException {
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+	
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+	}
+}
