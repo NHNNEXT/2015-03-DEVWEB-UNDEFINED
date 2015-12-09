80941696ae3dd56a4ab9e77bfce5c8c9f562a4f1
CHANT-MAC <cca_company@naver.com>
2015-12-09 09:41:34 +0900

    프로젝트 페이지

---
+++ Novelizer/webapp/js/project.js
@@ -0,0 +1,68 @@
+var id = 0;
+
+getProjectData(function(response){
+    var dataList = [];
+    for(var i = 0; i < response.length; ++i){
+        var data = response[i];
+        var list = $("<li class='project' data-id='"+data['projectId']+"'>"+data['projectName']+"</li>");
+        dataList.push(list);
+    }
+    $("#project-list").append(dataList);
+})
+
+$(".btn-add-project").click(function(){
+    var projectName = $(".project-name").val();
+    sendProjectData(projectName,function(response){
+        var dataList = [];
+        for(var i = 0; i < response.length; ++i){
+            var data = response[i];
+            var list = $("<li class='project' data-id='"+data['projectId']+"'>"+data['projectName']+"</li>");
+            dataList.push(list);
+        }
+        $("#project-list").append(dataList);
+    });
+})
+
+$("#project-list").on("click",".project",function(){
+    var projectId = $(this).data("id");
+    getSceneData(projectId,function(response){
+        $("#scene-list").empty();
+        var dataList = [];
+        for(var i = 0; i < response.length; ++i){
+            var data = response[i];
+            var list = $("<li class='scene'><a href='index.html?sceneId="+data['sceneId']+"'>"+data['sceneName']+"</a></li>");
+            dataList.push(list);
+        }
+        $("#scene-list").append(dataList);
+    });
+
+})
+
+function getProjectData(callback){
+    $.ajax({
+        url : "http://localhost:8080/project",
+        method : "GET",
+        dataType : "json",
+    })
+    .done(callback);
+};
+
+function sendProjectData(projectName, callback){
+    $.ajax({
+        url : "http://localhost:8080/project",
+        method : "POST",
+        dataType : "json",
+        data : {projectId:0, projectName : projectName},
+    })
+    .done(callback);
+};
+
+function getSceneData(projectId, callback){
+    $.ajax({
+        url : "http://localhost:8080/scene",
+        method : "GET",
+        dataType : "json",
+        data : {projectId:projectId}
+    })
+    .done(callback);
+}
