ea2e9ba11996966fbe09ea3b21ab8be67fb8b3f3
CHANT-MAC <cca_company@naver.com>
2015-12-14 19:48:13 +0900

    잉여파일 삭

--- Novelizer/src/model/character/Character.java
+++
@@ -1,6 +0,0 @@
-package model.character;
-
-public class Character {
-	int characterId;
-	String name;
-}
--- Novelizer/src/project/Project.java
+++
@@ -1,6 +0,0 @@
-package project;
-
-public class Project {
-	private int projectId;
-	private String projectName;
-}
