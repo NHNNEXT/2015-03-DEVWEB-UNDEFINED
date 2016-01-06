f20d2710c47b09d8f970e18c358c788cd4cd1ccb
GingerAebi <GingerAebi@gmail.com>
2016-01-06 12:53:30 +0900

    Project

---
+++ Novelizer/src/controller/ProjectsController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import service.ProjectService;
+
+@WebServlet("/newproject")
+public class ProjectsController extends HttpServlet {
+	private Logger log = LoggerFactory.getLogger(ProjectsController.class);
+
+	private ProjectService service;
+
+	@Override
+	public void init() throws ServletException {
+		service = new ProjectService();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userId = getUserId(req);
+		resp.getWriter().write(service.getProjectsByUserId(userId));
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String projectData = req.getParameter("projectData");
+		String userId = getUserId(req);
+		String result = "";
+		try {
+			log.info(projectData);
+			result += service.saveProject(projectData, userId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		resp.getWriter().write(result);
+	}
+
+	private String getUserId(HttpServletRequest req) {
+		HttpSession session = req.getSession();
+		return "" + session.getAttribute("userId");
+	}
+}
--- Novelizer/src/service/ProjectService.java
+++ Novelizer/src/service/ProjectService.java
@@ -1,11 +1,13 @@
 package service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import dao.ProjectDao;
+import dao.SceneDao;
 import model.Project;
 import utils.json.JsonHandler;
 
@@ -14,22 +16,29 @@ public class ProjectService {
 
 	private JsonHandler<Project> jsonHandler;
 	private ProjectDao projectDao;
+	private SceneDao sceneDao;
 
 	public ProjectService() {
 		jsonHandler = new JsonHandler<Project>();
 		projectDao = new ProjectDao();
+		sceneDao = new SceneDao();
 	}
 
 	public String getProjects() {
 		List<Project> projects = projectDao.selectAllProject();
 		log.info(jsonHandler.convertToJsonArray(projects));
 		return jsonHandler.convertToJsonArray(projects);
-
 	}
-	
+
 	public String getProjectsByUserId(String userId) {
 		List<Project> projects = projectDao.selectProjectsByUserId(userId);
 		return jsonHandler.convertToJsonArray(projects);
+	}
+	
+	public String getProject(int projectId) throws SQLException{
+		Project project = projectDao.selectProject(projectId);
+		project.setScenes(sceneDao.selectByProjectId(projectId));
+		return jsonHandler.convertToJson(project);
 	}
 
 	public int saveProject(String projectData, String userId) {
